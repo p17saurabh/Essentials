17a49da79d68dfbfcc3b0a2e9d4141a1d09ebf0d
Saurabh <p17saurabh@gmail.com>
2019-12-04 14:00:30 -0500

    final changes

--- src/app/pages/link/link.page.ts
+++ src/app/pages/link/link.page.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-link',
-  templateUrl: './link.page.html',
-  styleUrls: ['./link.page.scss'],
+  selector: "app-link",
+  templateUrl: "./link.page.html",
+  styleUrls: ["./link.page.scss"]
 })
 export class LinkPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
